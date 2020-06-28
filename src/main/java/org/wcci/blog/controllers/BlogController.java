df17aca24aed16bbc93a24c363553bcb77920bfa
Joe Grace <joeggrace@gmail.com>
2020-06-28 14:06:02 -0400

    all requirements completed

--- src/main/java/org/wcci/blog/controllers/BlogController.java
+++ src/main/java/org/wcci/blog/controllers/BlogController.java
@@ -5,25 +5,49 @@ import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.PostMapping;
+import org.wcci.blog.entities.Author;
 import org.wcci.blog.entities.Blog;
+import org.wcci.blog.entities.Category;
 import org.wcci.blog.entities.Tag;
+import org.wcci.blog.storage.AuthorStorage;
 import org.wcci.blog.storage.BlogStorage;
+import org.wcci.blog.storage.CategoryStorage;
 import org.wcci.blog.storage.TagStorage;
 
 @Controller
 public class BlogController {
     private BlogStorage blogStorage;
     private TagStorage tagStorage;
+    private CategoryStorage categoryStorage;
+    private AuthorStorage authorStorage;
 
-    public BlogController(BlogStorage blogStorage, TagStorage tagStorage) {
+    public BlogController(BlogStorage blogStorage, TagStorage tagStorage, CategoryStorage categoryStorage, AuthorStorage authorStorage) {
         this.blogStorage = blogStorage;
         this.tagStorage = tagStorage;
+        this.categoryStorage = categoryStorage;
+        this.authorStorage = authorStorage;
     }
 
     @GetMapping("blog/{blogTitle}")
     public String showSingleBlog(@PathVariable String blogTitle, Model model) {
         model.addAttribute("blog", blogStorage.findBlogByBlogTitle(blogTitle));
         return "blog-template";
+    }
+
+    @GetMapping("addablog")
+    public String addABlog(Model model) {
+        model.addAttribute("categories",categoryStorage.findAllCategories());
+        model.addAttribute("authors", authorStorage.findAllAuthors());
+        return "add-blog-template";
+    }
+
+    @PostMapping("/blog/addablogpost")
+    public String addABlogPost(String blogTitle, String blogBody, String name, String publishDate, String categoryName){
+        Author authorToBlog = authorStorage.findAuthorByName(name);
+        Category categoryToBlog = categoryStorage.findCategoryByName(categoryName);
+        Blog blogToAdd = new Blog(blogTitle,blogBody,authorToBlog,publishDate,categoryToBlog);
+        blogStorage.addBlog(blogToAdd);
+        return "redirect:/blog/" + blogTitle;
     }
 
     @PostMapping("/blog/addTag")
--- src/test/java/org/wcci/blog/controllers/BlogControllerTest.java
+++ src/test/java/org/wcci/blog/controllers/BlogControllerTest.java
@@ -4,7 +4,10 @@ import org.junit.jupiter.api.BeforeEach;
 import org.junit.jupiter.api.Test;
 import org.mockito.Mockito;
 import org.springframework.ui.Model;
+import org.wcci.blog.entities.Author;
+import org.wcci.blog.storage.AuthorStorage;
 import org.wcci.blog.storage.BlogStorage;
+import org.wcci.blog.storage.CategoryStorage;
 import org.wcci.blog.storage.TagStorage;
 
 import static org.assertj.core.api.Assertions.assertThat;
@@ -14,13 +17,17 @@ public class BlogControllerTest {
     private BlogStorage mockBlogStorage;
     private TagStorage mockTagStorage;
     private BlogController underTest;
+    private AuthorStorage mockAuthorStorage;
+    private CategoryStorage mockCategoryStorage;
     private Model model;
 
     @BeforeEach
     void setUp(){
         mockBlogStorage = mock(BlogStorage.class);
         mockTagStorage = mock(TagStorage.class);
-        underTest = new BlogController(mockBlogStorage, mockTagStorage);
+        mockAuthorStorage = mock(AuthorStorage.class);
+        mockCategoryStorage = mock(CategoryStorage.class);
+        underTest = new BlogController(mockBlogStorage, mockTagStorage, mockCategoryStorage, mockAuthorStorage);
         model = Mockito.mock(Model.class);
     }
 
@@ -28,6 +35,12 @@ public class BlogControllerTest {
     public void showBlogShouldShowSingleBlog(){
         String templateName = underTest.showSingleBlog("Blog Title", model);
         assertThat(templateName).isEqualTo("blog-template");
+    }
+
+    @Test
+    public void addBlogShouldShowAddBlogTemplate(){
+        String templateName = underTest.addABlog(model);
+        assertThat(templateName).isEqualTo("add-blog-template");
     }
 
 }
